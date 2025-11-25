1e414d01ed8693cef8d903297533433c3d0f7273
Winner <blessedwinner66@gmail.com>
2025-11-25 17:02:12 +0200

    Removed unnecessary lines

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
 import { ConfigService } from '@nestjs/config';
@@ -6,6 +6,7 @@ import { Role, User } from './entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt'
 
 
 @Injectable()
@@ -18,41 +19,44 @@ export class AuthService {
   ){}
  
   async signup(dto:SignupDto):Promise<{access_token:string}>{
+
+      
+      const hashed = await bcrypt.hash(dto.password,10)
+
+      const exists = await this.userRepo.findOneBy({ email:dto.email })
+      if(exists) throw new ConflictException("Email already in use")
+
       const user = this.userRepo.create({
         firstName:dto.firstName,
         lastName:dto.lastName,
         email:dto.email,
-        password:dto.password,
+        password:hashed,
         role:dto.role
       })
- 
-     if(!user){
-      throw new InternalServerErrorException("Signup failed") 
-     }
 
-     const access_token = await this.generateToken(user.id,user.email,user.role)
+     await this.userRepo.save(user)
+
+    const access_token = await this.generateToken(user.id,user.email,user.role)
      if(!access_token) throw new InternalServerErrorException("Failed to generate access token")
      return { access_token }
      
   }
+  
 
   private async generateToken(userId:string, email:string, role:Role){
       const accessPayload = { userId, email, role }
-      const accessSecret = this.config.get<string>('JWT_SECRET')
-      if(!accessSecret){
-         throw new InternalServerErrorException("Access Secret is not defined")
-      }
-      const accessToken  = await this.jwtService.signAsync(accessPayload,{ 
-        secret:this.config.get<string>('JWT_SECRET'),
-        expiresIn: '1d'
-      })
-
+      const accessToken  = await this.jwtService.signAsync(accessPayload,{expiresIn: '1d'})
       return accessToken
   }
 
   async login(dto:LoginDto):Promise<{access_token:string}>{
+
       const user = await this.userRepo.findOneBy({ email:dto.email })
       if(!user) throw new NotFoundException("The user does not exist in the database")
+
+      const isMatch = await bcrypt.compare(dto.password,user.password)
+      if(!isMatch) throw new UnauthorizedException("Invalid credentials")
+
       const access_token = await this.generateToken(user.id,user.email,user.role)
       if(!access_token){
         throw new InternalServerErrorException("Failed to generate access token")
