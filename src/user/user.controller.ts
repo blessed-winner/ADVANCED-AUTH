cb5a9ac85d8ec419c0983ce0da5adfe33fe2543b
Winner <blessedwinner66@gmail.com>
2025-12-01 06:05:57 +0200

    User controllers defined

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, InternalServerErrorException, Put, UnauthorizedException } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Request } from 'express';
 
+interface AuthenticatedRequest extends Request{
+  user?:{ id:string }
+}
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
  @Get('view/all')
-  findAll() {
-    return this.userService.findAll();
+  async findAll() {
+    return await this.userService.findAll();
   }
 
   @Get('view/:id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    return await this.userService.findOne(id);
   }
 
 
   @Delete('delete/:id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(id);
+  async delete(@Param('id') id:string){
+    return await this.userService.remove(id)
   }
+
+  @Get('profile')
+  async viewProfile(@Req() req:AuthenticatedRequest){
+     const userId = req?.user?.id;
+     if(!userId) throw new UnauthorizedException("Cannot get authenticated user")
+     return await this.userService.getProfile(userId)
+  }
+
+  @Put('profile')
+  async updateProfile(
+    @Req() req:AuthenticatedRequest,
+    @Body() dto:UpdateUserDto
+    ){
+    const userId = req?.user?.id;
+    if(!userId) throw new UnauthorizedException("Cannot get authenticated user")
+     return await this.userService.updateProfile(userId,dto)
+  }
+ 
 }
